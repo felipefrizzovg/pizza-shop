1b56063522d599d7478cf4f287d1612363959978
Felipe Frizzo <felipefrizzovg@gmail.com>
2024-08-04 16:39:11 -0300

    Layouts da aplicação e erros de linting

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,7 @@
-import { RouterProvider } from 'react-router-dom'
 import './global.css'
+
+import { RouterProvider } from 'react-router-dom'
+
 import { router } from './routes'
 
 export function App() {
