0872085b5c45a835e99c0be95353ffad5a10d808
Felipe Frizzo <felipefrizzovg@gmail.com>
2024-08-16 10:33:58 -0300

    Loading detalhe do pedido

--- src/pages/app/orders/order-details.tsx
+++ src/pages/app/orders/order-details.tsx
@@ -19,6 +19,7 @@ import {
   TableHeader,
   TableRow,
 } from '@/components/ui/table'
+import { OrderDetailsSkeleton } from './order-details-skeleton'
 
 export interface OrderDetailProps {
   orderId: string
@@ -39,7 +40,7 @@ export function OrderDetails({ orderId, open }: OrderDetailProps) {
         <DialogDescription>Detalhes do pedido</DialogDescription>
       </DialogHeader>
 
-      {order && (
+      {order ? (
         <div className="space-y-6">
           <Table>
             <TableBody>
@@ -154,6 +155,8 @@ export function OrderDetails({ orderId, open }: OrderDetailProps) {
             </TableFooter>
           </Table>
         </div>
+      ) : (
+        <OrderDetailsSkeleton />
       )}
     </DialogContent>
   )
